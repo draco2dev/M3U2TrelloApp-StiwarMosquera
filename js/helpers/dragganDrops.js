ff7373c8d6a53eab9b17f356b5158a08a95ed252
Stiwar Mosquera <stiwarmg@gmail.com>
2022-07-30 23:25:49 -0500

    Actualizacion

--- js/helpers/dragganDrops.js
+++ js/helpers/dragganDrops.js
@@ -1,14 +1,6 @@
-// const sor = new Sortable.default(
-//     document.querySelectorAll('.kanban__dropzone'),
-//     { draggable: 'article'}
-// )
+const sor = new Sortable.default(
+    document.querySelectorAll('.kanban__dropzone'),
+    { draggable: 'article'}
+)
 // columnas por la cual se moveran.
-
-const container = document.querySelector("main")
-
-const draggable = new Sortable.default(container,{
-    draggable:"div"
-})
-
-draggable.on("drag:start", (e)=> console.log(e))
 
